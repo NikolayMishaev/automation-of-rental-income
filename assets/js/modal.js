e7df21f4912b0274d3a27c5e8d3d406b9fac5ccb
Lidiya.Shananina <lidiya.shananina@x5.ru>
2022-03-21 12:04:57 +0300

    feat: Доделаны модальные окна

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -7,16 +7,25 @@ offerBtn.onclick = function () {
   modal.style.display = "flex";
 };
 
-closeBtn.onclick = function () {
-  modal.style.display = "none";
-};
-
-cancelBtn.onclick = function () {
-  modal.style.display = "none";
-};
-
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  if (event.target == modalForm) {
+    modalForm.style.display = "none";
+  }
+};
+
+const modalForm = document.querySelector(".modal-form");
+const contactBtn = document.querySelector(".info-card__primary-btn");
+
+contactBtn.onclick = function () {
+  modalForm.style.display = "flex";
+};
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+  modalForm.style.display = "none";
+};
+cancelBtn.onclick = function () {
+  modal.style.display = "none";
 };
