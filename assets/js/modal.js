e2718557b4d49db1bf2fb0c4a68156b2cbb6792b
Lidiya.Shananina <lidiya.shananina@x5.ru>
2022-03-22 17:59:53 +0300

    feat: Верстка слайдера

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,31 +1,42 @@
-const cancelBtn = document.querySelector(".modal__cancel-btn");
-const closeBtn = document.querySelector(".modal__close-btn");
-const modal = document.querySelector(".modal");
-const offerBtn = document.querySelector(".info-card__offer-btn");
+const cancelBtn = document.querySelectorAll(".modal__cancel-btn");
+const closeBtn = document.querySelectorAll(".modal__close-btn");
+const modalOfferPrice = document.querySelector(".modal-offer-price");
+const offerBtn = document.querySelectorAll(".info-card__offer-btn");
+const modalForm = document.querySelector(".modal-form");
+const contactBtn = document.querySelectorAll(".info-card__primary-btn");
 
-offerBtn.onclick = function () {
-  modal.style.display = "flex";
-};
+for (let index = 0; index < offerBtn.length; index++) {
+  offerBtn[index].addEventListener("click", function () {
+    modalOfferPrice.style.display = "flex";
+  });
+}
+for (let index = 0; index < contactBtn.length; index++) {
+  contactBtn[index].addEventListener("click", function () {
+    modalForm.style.display = "flex";
+  });
+}
+for (let index = 0; index < closeBtn.length; index++) {
+  closeBtn[index].addEventListener("click", function () {
+    modalOfferPrice.style.display = "none";
+    modalForm.style.display = "none";
+  });
+}
+for (let index = 0; index < cancelBtn.length; index++) {
+  cancelBtn[index].addEventListener("click", function () {
+    modalOfferPrice.style.display = "none";
+    modalForm.style.display = "none";
+  });
+}
 
 window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modalOfferPrice.style.display = "none";
   }
   if (event.target == modalForm) {
     modalForm.style.display = "none";
   }
 };
 
-const modalForm = document.querySelector(".modal-form");
-const contactBtn = document.querySelector(".info-card__primary-btn");
-
 contactBtn.onclick = function () {
   modalForm.style.display = "flex";
-};
-closeBtn.onclick = function () {
-  modal.style.display = "none";
-  modalForm.style.display = "none";
-};
-cancelBtn.onclick = function () {
-  modal.style.display = "none";
 };
