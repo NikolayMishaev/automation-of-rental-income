da85698b575f166ba9e4c52c128e5454e349ecb0
Nikolay.Mishaev <nikolay.mishaev@x5.ru>
2022-03-22 19:56:28 +0300

    feat: Навешены обработчики на кнопки авторизации - демонстрация блоков для авторизированных и нет пользователей

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -1,5 +1,8 @@
 const headerCity = document.querySelector(".header__city");
 const modalWidow = document.querySelector(".modal");
+const signInButton = document.querySelectorAll(".menu__link");
+const menuLinks = document.querySelector(".menu__links");
+const menuProfile = document.querySelector(".menu__profile");
 
 headerCity.addEventListener("click", (e) => {
     modalWidow.style.opacity = "1";
@@ -8,3 +11,10 @@ headerCity.addEventListener("click", (e) => {
 modalWidow.addEventListener("click", (e) => {
     if (e.target.classList.contains("modal")) modalWidow.style.opacity = "0";
 });
+
+signInButton.forEach((i) =>
+    i.addEventListener("click", (e) => {
+        menuLinks.style.display = "none";
+        menuProfile.style.display = "flex";
+    })
+);
