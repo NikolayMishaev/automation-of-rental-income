0884e7d4d432a80e508197a0ce081c6df8ff3e5c
Nikolay.Mishaev <nikolay.mishaev@x5.ru>
2022-03-23 12:28:47 +0300

    feat: Новый попап для выбора городов

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -3,6 +3,7 @@ const modalWidow = document.querySelector(".modal");
 const signInButton = document.querySelectorAll(".menu__link");
 const menuLinks = document.querySelector(".menu__links");
 const menuProfile = document.querySelector(".menu__profile");
+const modalCloseMain = document.querySelector(".modal__close-btn");
 
 headerCity.addEventListener("click", (e) => {
     modalWidow.style.opacity = "1";
@@ -12,11 +13,21 @@ headerCity.addEventListener("click", (e) => {
 
 modalWidow.addEventListener("click", (e) => {
     if (e.target.classList.contains("modal")) {
-        modalWidow.style.opacity = "0";
-        modalWidow.style.zIndex = "-1";
-        modalWidow.style.transform = "scale(4)";
+        closePopup();
     }
 });
+
+modalCloseMain.addEventListener("click", (e) => {
+    if (e.target.closest(".modal__close-btn")) {
+        closePopup();
+    }
+});
+
+function closePopup() {
+    modalWidow.style.opacity = "0";
+    modalWidow.style.zIndex = "-1";
+    modalWidow.style.transform = "scale(4)";
+}
 
 signInButton.forEach((i) =>
     i.addEventListener("click", (e) => {
