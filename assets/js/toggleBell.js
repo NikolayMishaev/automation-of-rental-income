a06937012c606c417d31c0c518b8806f999907e4
Lidiya.Shananina <lidiya.shananina@x5.ru>
2022-03-24 16:43:29 +0300

    feat: Удалены лищнике js файлы

--- assets/js/toggleBell.js
+++
@@ -1,7 +0,0 @@
-const bellBtn = document.querySelectorAll(".info-card__icon-bell");
-
-for (let index = 0; index < bellBtn.length; index++) {
-  bellBtn[index].addEventListener("click", function () {
-    this.classList.toggle("active");
-  });
-}
