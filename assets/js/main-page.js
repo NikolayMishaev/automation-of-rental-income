8a02277921b5c4eea7f425c83d6cc857325f1953
Nikolay.Mishaev <nikolay.mishaev@x5.ru>
2022-03-25 11:58:34 +0300

    feat: Логика работы кнопок сортировки - логика работы в мобильной и десктопной версиях

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -1,4 +1,9 @@
 (function () {
+    // стейт
+    const stateMain = {
+        currentOpenLeftBlock: null,
+    };
+
     // логика открытия/закрытия попапа выбора городов "Ваш город"
     const headerCity = document.querySelector(".header__city");
     const modalWidow = document.querySelector(".modal");
@@ -42,12 +47,15 @@
                 btnMap.classList.add("main-btn_active");
                 cardsContainer.style.display = "none";
                 mapContainer.style.display = "block";
+                stateMain.currentOpenLeftBlock = cardsContainer;
             } else {
                 btnList.classList.add("main-btn_active");
                 btnMap.classList.remove("main-btn_active");
                 cardsContainer.style.display = "block";
                 mapContainer.style.display = "none";
+                stateMain.currentOpenLeftBlock = mapContainer;
             }
+            console.log(stateMain.currentOpenLeftBlock);
         })
     );
 
@@ -69,18 +77,54 @@
     const currentValueNumberCards =
         document.querySelector("#main-number-cards");
 
+    const mainSubmenuNumberCards = document.querySelector(
+        ".main-submenu_style_number-cards"
+    );
+    const mainSubmenuSortDesktop = document.querySelector(
+        ".main-submenu_style_sort"
+    );
+    const mainSubmenuSortMobile = document.querySelector(
+        ".main-submenu_style_sort-mobile"
+    );
+
+    // при наведении на контейнеры содерщащие подменю, удаляем миксовый класс у подменю
+    const mainContainerNumberCards = document.querySelector(
+        "#main-search-number-cards"
+    );
+    const mainContainerSearchDesktop = document.querySelector(
+        "#main-search-sort-cards-desktop"
+    );
+    const mainContainerSearchMobile = document.querySelector(
+        "#main-search-sort-cards-mobile"
+    );
+    const arrayWithContainersSort = [
+        mainContainerNumberCards,
+        mainContainerSearchDesktop,
+        mainContainerSearchMobile,
+    ];
+    arrayWithContainersSort.forEach((i) =>
+        i.addEventListener("mouseover", function () {
+            mainSubmenuNumberCards.classList.remove("mix-hidden");
+            mainSubmenuSortDesktop.classList.remove("mix-hidden");
+            mainSubmenuSortMobile.classList.remove("mix-hidden");
+        })
+    );
+
     itemsSubmenuSort.forEach((i) =>
         i.addEventListener("click", (e) => {
             if (e.target.ariaLabel === "number-cards") {
                 deleteActiveClass("number-cards");
                 e.target.classList.add("main-submenu__item_active");
                 currentValueNumberCards.textContent = e.target.textContent;
+                mainSubmenuNumberCards.classList.add("mix-hidden");
             }
             if (e.target.ariaLabel === "sort-desktop") {
                 deleteActiveClass("sort-desktop");
                 e.target.classList.add("main-submenu__item_active");
                 addActiveClass(e.target.textContent);
                 currentValueSort.textContent = e.target.textContent;
+                mainSubmenuSortDesktop.classList.add("mix-hidden");
+                mainSubmenuSortMobile.classList.add("mix-hidden");
             }
         })
     );
@@ -127,5 +171,66 @@
         iconSortBlock.classList.remove(
             "main__control-panel-icon_search-active"
         );
+    });
+
+    // логика кнопки фильтров по клику на иконку с фильтрами в мобильной версии
+    const iconFilters = document.querySelector(
+        ".main__control-panel-icon_filters"
+    );
+    const leftMainColumnPrice = document.querySelector(
+        ".main__column_type_price"
+    );
+    const rightMainColumnFilters = document.querySelector(
+        ".main__column_type_filters"
+    );
+
+    iconFilters.addEventListener("click", (e) => {
+        if (
+            !iconFilters.classList.contains(
+                "main__control-panel-icon_filters-active"
+            )
+        ) {
+            leftMainColumnPrice.style.display = "none";
+            rightMainColumnFilters.style.display = "block";
+            iconFilters.classList.add(
+                "main__control-panel-icon_filters-active"
+            );
+        } else {
+            leftMainColumnPrice.style.display = "block";
+            rightMainColumnFilters.style.display = "none";
+            iconFilters.classList.remove(
+                "main__control-panel-icon_filters-active"
+            );
+        }
+    });
+
+    window.addEventListener("resize", function (e) {
+        if (
+            e.target.innerWidth > 1500 &&
+            iconFilters.classList.contains(
+                "main__control-panel-icon_filters-active"
+            )
+        ) {
+            leftMainColumnPrice.style.display = "block";
+        } else if (
+            e.target.innerWidth < 1500 &&
+            iconFilters.classList.contains(
+                "main__control-panel-icon_filters-active"
+            )
+        ) {
+            leftMainColumnPrice.style.display = "none";
+        }
+        if (e.target.innerWidth > 1500) {
+            rightMainColumnFilters.style.display = "block";
+        }
+    });
+
+    // тоггл лайка по клику на сердечко в карточке
+    const mainCardsContainer = document.querySelector(".main__cards-container");
+
+    mainCardsContainer.addEventListener("click", (e) => {
+        if (e.target.classList.contains("card-price__like")) {
+            e.target.classList.toggle("card-price__like_active");
+        }
     });
 })();
