34ecd900f7ef255e87fd4721f1e22e0c63084941
Lidiya.Shananina <lidiya.shananina@x5.ru>
2022-03-27 17:00:03 +0300

    fix: Переписаны обработчики на модальных окнах. Теперь делегирование

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-const cancelBtn = document.querySelectorAll(".modal__cancel-btn");
-const closeBtn = document.querySelectorAll(".modal__close-btn");
 const modalOfferPrice = document.querySelector(".modal-offer-price");
 const offerBtn = document.querySelectorAll(".info-card__offer-btn");
 const modalForm = document.querySelector(".modal-form");
@@ -15,27 +13,18 @@ for (let index = 0; index < contactBtn.length; index++) {
     modalForm.style.display = "flex";
   });
 }
-// for (let index = 0; index < closeBtn.length; index++) {
-//   closeBtn[index].addEventListener("click", function () {
-//     modalOfferPrice.style.display = "none";
-//     modalForm.style.display = "none";
-//   });
-// }
-// for (let index = 0; index < cancelBtn.length; index++) {
-//   cancelBtn[index].addEventListener("click", function () {
-//     modalOfferPrice.style.display = "none";
-//     modalForm.style.display = "none";
-//   });
-// }
 
-window.onclick = function (event) {
-  if (event.target == modalOfferPrice) {
+modalOfferPrice.addEventListener("click", (e) => {
+  if (e.target.closest(".modal__close-btn") || e.target.closest(".modal__cancel-btn")) {
     modalOfferPrice.style.display = "none";
   }
-  if (event.target == modalForm) {
+});
+
+modalForm.addEventListener("click", (e) => {
+  if (e.target.closest(".modal__close-btn") || e.target.closest(".modal__cancel-btn")) {
     modalForm.style.display = "none";
   }
-};
+});
 
 contactBtn.onclick = function () {
   modalForm.style.display = "flex";
