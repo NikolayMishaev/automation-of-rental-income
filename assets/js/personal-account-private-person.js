e22d4358aa95dbc8f263d2820fb7befe7bd5f1bc
Nikolay.Mishaev <nikolay.mishaev@x5.ru>
2022-03-30 00:16:46 +0300

    feat: Настройка адаптива

--- assets/js/personal-account-private-person.js
+++ assets/js/personal-account-private-person.js
@@ -34,4 +34,46 @@
     function deleteActiveClass(element, className) {
         element.classList.remove(className);
     }
+
+    // логика переключения список, карточки
+    const buttonsChangeViewContainer = document.querySelector(
+        ".prof-control-panel__buttons-container"
+    );
+
+    const buttonList = document.querySelector("#button-list");
+    const buttonCards = document.querySelector("#button-cards");
+
+    const cardsContainer = document.querySelector(
+        ".prof-general__cards-container"
+    );
+    const tableContainer = document.querySelector(".prof-table");
+
+    buttonsChangeViewContainer.addEventListener("click", (e) => {
+        if (e.target.ariaLabel === "button-list") {
+            e.target.classList.add("prof-control-panel__button_active");
+            buttonCards.classList.remove("prof-control-panel__button_active");
+            tableContainer.classList.remove("mix-display-none");
+            cardsContainer.classList.add("mix-display-none");
+        } else {
+            e.target.classList.add("prof-control-panel__button_active");
+            buttonList.classList.remove("prof-control-panel__button_active");
+            hideTable();
+        }
+    });
+
+    // логика сркытия таблицы при первой загрузке
+    if (window.innerWidth < 1151) {
+        hideTable();
+    }
+
+    function hideTable() {
+        tableContainer.classList.add("mix-display-none");
+        cardsContainer.classList.remove("mix-display-none");
+    }
+
+    window.addEventListener("resize", function (e) {
+        if (e.target.innerWidth < 1151) {
+            hideTable();
+        }
+    });
 })();
