c3061243a8ee7ea099e3c2364d09c1da8c230c38
Nikolay.Mishaev <nikolay.mishaev@x5.ru>
2022-04-02 17:29:26 +0300

    AWP-37: feat: Верстка вкладки Арихв + логика - Верстка контента по вкладке "Архив" - Переписан блок кода с переключением табов - Созданы общие функции для переиспользования

--- assets/js/common-JS-to-all-pages.js
+++ assets/js/common-JS-to-all-pages.js
@@ -1,51 +1,77 @@
-(function () {
-    // логика открытия/закрытия попапа выбора городов "Ваш город"
+export { addClassElement, removeClassElement, switchButtons };
 
-    const popupOverlay = document.querySelector(".main-popup");
+// общие функции
+function addClassElement(element, className) {
+    element.classList.add(className);
+}
 
-    const headerCity = document.querySelector(".header__city");
-    const popupChangeCity = document.querySelector(
-        ".main-popup_type_change-city"
-    );
+function removeClassElement(element, className) {
+    element.classList.remove(className);
+}
 
-    popupOverlay.addEventListener("click", (e) => {
-        if (
-            e.target.classList.contains("main-popup") ||
-            e.target.classList.contains("main-popup__close")
-        ) {
-            popupChangeCity.classList.remove("mix-visible-scale");
+function switchButtons(
+    eventTarget,
+    arrayButtons,
+    arrayContent,
+    activeClassButton,
+    notActiveClassContent
+) {
+    if (eventTarget.closest(`.${activeClassButton}`)) {
+        return;
+    }
+    arrayButtons.forEach((i) => removeClassElement(i, activeClassButton));
+    addClassElement(eventTarget, activeClassButton);
+    arrayContent.forEach((i) => {
+        if (i.ariaLabel === eventTarget.ariaLabel) {
+            removeClassElement(i, notActiveClassContent);
+        } else {
+            addClassElement(i, notActiveClassContent);
         }
     });
+}
 
-    headerCity.addEventListener("click", (e) => {
-        popupChangeCity.classList.add("mix-visible-scale");
-    });
+// логика открытия/закрытия попапа выбора городов "Ваш город"
 
-    // логика авторизации по клику на "Регистрации" и "Вход" в Header, пока не готовы попапы регистрации
-    const linkSignIn = document.querySelector("#link-signIn");
-    const buttonClosePopup = document.querySelector(
-        "#button-close-popup-sign-in"
-    );
-    const formSignIn = document.querySelector("#form-sign-in");
+const popupOverlay = document.querySelector(".main-popup");
 
-    const menuLinks = document.querySelector(".menu__links");
-    const menuProfile = document.querySelector(".menu__profile");
-    const popupLogIn = document.querySelector(".modal-login");
+const headerCity = document.querySelector(".header__city");
+const popupChangeCity = document.querySelector(".main-popup_type_change-city");
 
-    formSignIn.addEventListener("submit", (e) => {
-        e.preventDefault();
-        menuLinks.style.display = "none";
-        menuProfile.style.display = "flex";
+popupOverlay.addEventListener("click", (e) => {
+    if (
+        e.target.classList.contains("main-popup") ||
+        e.target.classList.contains("main-popup__close")
+    ) {
+        popupChangeCity.classList.remove("mix-visible-scale");
+    }
+});
+
+headerCity.addEventListener("click", (e) => {
+    popupChangeCity.classList.add("mix-visible-scale");
+});
+
+// логика авторизации по клику на "Регистрации" и "Вход" в Header, пока не готовы попапы регистрации
+const linkSignIn = document.querySelector("#link-signIn");
+const buttonClosePopup = document.querySelector("#button-close-popup-sign-in");
+const formSignIn = document.querySelector("#form-sign-in");
+
+const menuLinks = document.querySelector(".menu__links");
+const menuProfile = document.querySelector(".menu__profile");
+const popupLogIn = document.querySelector(".modal-login");
+
+formSignIn.addEventListener("submit", (e) => {
+    e.preventDefault();
+    menuLinks.style.display = "none";
+    menuProfile.style.display = "flex";
+    popupLogIn.classList.remove("mix-display-flex");
+});
+
+buttonClosePopup.addEventListener("click", (e) => {
+    if (e.target.closest(".modal__close-btn")) {
         popupLogIn.classList.remove("mix-display-flex");
-    });
+    }
+});
 
-    buttonClosePopup.addEventListener("click", (e) => {
-        if (e.target.closest(".modal__close-btn")) {
-            popupLogIn.classList.remove("mix-display-flex");
-        }
-    });
-
-    linkSignIn.addEventListener("click", (e) => {
-        popupLogIn.classList.add("mix-display-flex");
-    });
-})();
+linkSignIn.addEventListener("click", (e) => {
+    popupLogIn.classList.add("mix-display-flex");
+});
