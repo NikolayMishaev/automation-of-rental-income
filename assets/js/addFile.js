da34945191f4bd9a5261e3ae8d68298563215c06
Lidiya.Shananina <lidiya.shananina@x5.ru>
2022-04-04 11:02:20 +0300

    feat: Добавлен код для нескольких файлов

--- assets/js/addFile.js
+++ assets/js/addFile.js
@@ -1,32 +1,48 @@
-const addModalBtn = document.querySelector(".modal-edit-card-object__btn input");
+const addModalBtnContainer = document.querySelector(".modal-edit-card-objec__add-btn");
+const addModalBtn = addModalBtnContainer.querySelector("input");
 const photoContainer = document.querySelector(".modal-edit-card-object__photo-container");
 
+// логика для отображения файлов с фото
 addModalBtn.addEventListener("change", (e) => {
-  const newFile = document.createElement("div");
-  const image = document.createElement("img");
-  const imageContainer = document.createElement("div");
-  const checkboxContainer = document.createElement("label");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  newFile.classList.add("modal-edit-card-object__photo-container-item");
-  let file = e.currentTarget.files[0];
-  let reader = new FileReader();
-  reader.onloadend = function () {
-    image.src = reader.result;
-  };
+  for (let index = 0; index < e.currentTarget.files.length; index++) {
+    let file = e.currentTarget.files[index];
+    const newFile = document.createElement("div");
+    const image = document.createElement("img");
+    const imageContainer = document.createElement("div");
+    const checkboxContainer = document.createElement("label");
+    const checkbox = document.createElement("input");
 
-  if (file) {
-    reader.readAsDataURL(file);
-  } else {
-    image.src = "";
+    checkbox.type = "checkbox";
+    newFile.classList.add("modal-edit-card-object__photo-container-item");
+    // предосмотр фото
+    let reader = new FileReader();
+    reader.onloadend = function () {
+      image.src = reader.result;
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      image.src = "";
+    }
+    imageContainer.append(image);
+    checkboxContainer.textContent = file.name;
+    imageContainer.classList.add("modal-edit-card-object__img");
+    checkboxContainer.classList.add("checkbox-container");
+    checkbox.classList.add("checkbox-container__checkbox");
+    newFile.append(imageContainer);
+    checkboxContainer.prepend(checkbox);
+    newFile.append(checkboxContainer);
+    photoContainer.append(newFile);
+    setNewInput(addModalBtnContainer, addModalBtn);
   }
-  imageContainer.append(image);
-  checkboxContainer.textContent = file.name;
-  imageContainer.classList.add("modal-edit-card-object__img");
-  checkboxContainer.classList.add("checkbox-container");
-  checkbox.classList.add("checkbox-container__checkbox");
-  newFile.append(imageContainer);
-  checkboxContainer.prepend(checkbox);
-  newFile.append(checkboxContainer);
-  photoContainer.append(newFile);
 });
+
+function setNewInput(btn, input) {
+  input.classList.add("modal-edit-card-object__input-file_full");
+  const newInput = document.createElement("input");
+  newInput.setAttribute("type", "file");
+  newInput.setAttribute("multiple", "true");
+  newInput.setAttribute("accept", "image/*");
+  btn.append(newInput);
+}
