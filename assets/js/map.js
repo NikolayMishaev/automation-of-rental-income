e9c00507d41124bc8bdf1335388fe68fb65f356c
Lidiya.Shananina <lidiya.shananina@x5.ru>
2022-04-05 16:11:31 +0300

    AWP-28: Добавлена библиотека и точке без api

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,27 +1,26 @@
 let map = null;
 let marker = null;
 
-let myIcon = L.divIcon({
+function myIcon(price) {
+  if (!price) price = 44444;
+  return L.divIcon({
+    className: "custom-div-icon",
+    html: `<h3>${price} р</h3><div class='custom-div-icon__arrow'></div>`,
+    iconSize: [98, 40],
+    iconAnchor: [50, 52],
+  });
+}
+
+let myIconClear = L.divIcon({
   className: "custom-div-icon",
   html: "<h3>44 520р</h3><div class='custom-div-icon__arrow'></div>",
-  iconSize: [98, 40],
-  iconAnchor: [15, 42],
+  iconSize: [1, 1],
+  iconAnchor: [1, 1],
 });
 
-let cities = {
-  Moscow: {
-    lat: "55.7522200",
-    lon: "37.6155600",
-  },
-  "Saint-Petersburg": {
-    lat: "59.9386300",
-    lon: "30.3141300",
-  },
-  Ekaterinburg: {
-    lat: "56.8519000",
-    lon: "60.6122000",
-  },
-};
+function tooltip(obj) {
+  return `<div class="map__tooltip_current"><div class="map__img"><img src="${"./assets/img/slider-item.jpg"}" alt="" /></div><div class="map__tooltip-info"><h4>${"Москва, Ул. Льва Толстого, дом 23, корп.1"}</h4><h5>${"44 287"} ₽ в месяц</h5></div></div>`;
+}
 
 const googleLayer =
   ("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
@@ -36,85 +35,63 @@ const openLayer =
     attribution:
       '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   });
-// функция принимает позицию - массив с широтой и долготой
-// и сообщение, отображаемое над маркером (tooltip)
-function getMap(position, tooltip) {
-  // если карта не была инициализирована
-  if (map === null) {
-    // второй аргумент, принимаемый методом setView - это масштаб (zoom)
-    map = L.map("map").setView(position, 15);
-  } else {
-    // map.flyTo(position);
+
+const idObjectOnMap = document.querySelector("#map").dataset.id;
+
+async function getObjectPoint(id) {
+  let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  if (response.ok) {
+    let result = await response.json();
+    // TODO тут нужны будут координаты массивом
+    let coord = [55.7585386, 37.6116878];
+    setMarker(coord);
+    setOuterMarkers(objectList);
   }
-  L.marker(position, { icon: myIcon })
+}
+getObjectPoint(idObjectOnMap);
+
+async function getObjectListPounts() {
+  let response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+}
+
+let objectList = {
+  0: {
+    lat: "55.7590447",
+    lon: "37.6155600",
+    price: 50000,
+  },
+  1: {
+    lat: "55.7590447",
+    lon: "37.614094",
+    price: 55555,
+  },
+};
+
+// повесить слой карты
+
+// установить начальную точку
+
+function setMarker(coord) {
+  map = L.map("map").setView(coord, 17);
+  marker = L.marker(coord, { icon: myIcon(54444) })
     .addTo(map)
-    .bindPopup(tooltip)
+    .bindPopup(tooltip())
     .openPopup();
-  // без этого карта работать не будет
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
   }).addTo(map);
-
-  // добавляем маркер с сообщением
-  // L.marker(position, { icon: myIcon })
-  //   .addTo(map)
-  //   .bindPopup(tooltip)
-  //   .openPopup();
 }
 
-// получить текущее местоположение
-
-function getPosition() {
-  navigator.geolocation.getCurrentPosition(success, error, {
-    enableHighAccuracy: true,
-  });
+function setOuterMarkers(list) {
+  for (const key in list) {
+    L.marker([+list[key].lat, +list[key].lon], { icon: myIcon() })
+      .addTo(map)
+      .on("click", () =>
+        L.popup()
+          .setLatLng([+list[key].lat, +list[key].lon])
+          .setContent(tooltip())
+          .openOn(map)
+      );
+  }
 }
-
-document.addEventListener("DOMContentLoaded", getPosition);
-
-function success({ coords }) {
-  const { latitude, longitude } = coords;
-  const currentPosition = [latitude, longitude];
-  // вызываем функцию, передавая ей текущую позицию и сообщение
-  getMap(
-    currentPosition,
-    ' <div class="map__tooltip_current"><div class="map__img"><img src="./assets/img/slider-item.jpg" alt="" /></div><div class="map__tooltip-info"><h4>Москва, Ул. Льва Толстого, дом 23, корп.1</h4><h5>44 287 ₽ в месяц</h5></div></div>'
-  );
-  getMap(
-    [cities.Ekaterinburg.lat, cities.Ekaterinburg.lon],
-    ' <div class="map__tooltip_current"><div class="map__img"><img src="./assets/img/slider-item.jpg" alt="" /></div><div class="map__tooltip-info"><h4>Москва, Ул. Льва Толстого, дом 23, корп.1</h4><h5>44 287 ₽ в месяц</h5></div></div>'
-  );
-}
-
-function error({ message }) {
-  console.log(message);
-}
-const $cities = document.getElementById("cities");
-// for (const city in cities) {
-//   // создаем кнопку
-
-//   // текстовое содержимое кнопки - название города
-
-//   // получаем широту и долготу
-//   const { lat, lon } = cities[city];
-
-//   // записываем название города, широту и долготу
-//   // в соответствующие data-атрибуты
-//   $button.dataset.city = city;
-//   $button.dataset.lat = lat;
-//   $button.dataset.lon = lon;
-
-//   // добавляем кнопку в контейнер
-//   $cities.append($button);
-// }
-
-$cities.addEventListener("click", ({ target }) => {
-  // нас интересует только нажатие кнопки
-
-  // получаем название города, широту и долготу из data-атрибутов
-  const { city, lat, lon } = target.dataset;
-  const position = [lat, lon];
-  // вызываем функцию, передавая ей позицию и название города
-  getMap(position, city);
-});
