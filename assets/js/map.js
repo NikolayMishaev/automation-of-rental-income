bdd7e8b78d1f9a4bd20cebca1f79f23cb33cedbd
Lidiya.Shananina <lidiya.shananina@x5.ru>
2022-04-05 17:16:55 +0300

    AWP-28: Отредактированы запросы

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,7 +2,6 @@ let map = null;
 let marker = null;
 
 function myIcon(price) {
-  if (!price) price = 44444;
   return L.divIcon({
     className: "custom-div-icon",
     html: `<h3>${price} р</h3><div class='custom-div-icon__arrow'></div>`,
@@ -11,71 +10,68 @@ function myIcon(price) {
   });
 }
 
-let myIconClear = L.divIcon({
-  className: "custom-div-icon",
-  html: "<h3>44 520р</h3><div class='custom-div-icon__arrow'></div>",
-  iconSize: [1, 1],
-  iconAnchor: [1, 1],
-});
-
 function tooltip(obj) {
-  return `<div class="map__tooltip_current"><div class="map__img"><img src="${"./assets/img/slider-item.jpg"}" alt="" /></div><div class="map__tooltip-info"><h4>${"Москва, Ул. Льва Толстого, дом 23, корп.1"}</h4><h5>${"44 287"} ₽ в месяц</h5></div></div>`;
+  //TODO нужнен url картинки
+  return `<div class="map__tooltip_current"><div class="map__img"><img src="${"./assets/img/slider-item.jpg"}" alt="" /></div><div class="map__tooltip-info"><h4>${
+    obj.title
+  }</h4><h5>${obj.price} ₽ в месяц</h5></div></div>`;
 }
 
-const googleLayer =
-  ("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
-  {
-    maxZoom: 20,
-    subdomains: ["mt0", "mt1", "mt2", "mt3"],
-  });
-
-const openLayer =
-  ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    attribution:
-      '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  });
-
+// ид объекта
 const idObjectOnMap = document.querySelector("#map").dataset.id;
+
+// информация о текущем объекте
 
 async function getObjectPoint(id) {
   let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
   if (response.ok) {
     let result = await response.json();
-    // TODO тут нужны будут координаты массивом
-    let coord = [55.7585386, 37.6116878];
-    setMarker(coord);
-    setOuterMarkers(objectList);
+    // TODO удалить result
+    result = {
+      lat: "55.7590447",
+      lon: "37.6175600",
+      price: 50000,
+      title: "Москва, Ул. Льва Толстого, дом 23, корп.1",
+    };
+    setMarker(result);
   }
 }
 getObjectPoint(idObjectOnMap);
 
+// получить остальные точки
 async function getObjectListPounts() {
   let response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+  if (response.ok) {
+    let result = await response.json();
+    // TODO удалить result
+    result = {
+      0: {
+        lat: "55.7590447",
+        lon: "37.6155600",
+        price: 40000,
+        photo: "",
+        title: "Москва, Ул. Льва Толстого, дом 23, корп.1",
+      },
+      1: {
+        lat: "55.7590447",
+        lon: "37.614094",
+        price: 35555,
+        title: "Москва, Ул. Льва Толстого, дом 23, корп.1",
+      },
+    };
+    setOuterMarkers(result);
+  }
 }
-
-let objectList = {
-  0: {
-    lat: "55.7590447",
-    lon: "37.6155600",
-    price: 50000,
-  },
-  1: {
-    lat: "55.7590447",
-    lon: "37.614094",
-    price: 55555,
-  },
-};
-
-// повесить слой карты
+getObjectListPounts();
 
 // установить начальную точку
 
-function setMarker(coord) {
+function setMarker(obj) {
+  let coord = [+obj.lat, +obj.lon];
   map = L.map("map").setView(coord, 17);
-  marker = L.marker(coord, { icon: myIcon(54444) })
+  marker = L.marker(coord, { icon: myIcon(obj.price) })
     .addTo(map)
-    .bindPopup(tooltip())
+    .bindPopup(tooltip(obj))
     .openPopup();
   L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
     maxZoom: 20,
@@ -83,15 +79,12 @@ function setMarker(coord) {
   }).addTo(map);
 }
 
+// установить точки
+
 function setOuterMarkers(list) {
   for (const key in list) {
-    L.marker([+list[key].lat, +list[key].lon], { icon: myIcon() })
+    L.marker([+list[key].lat, +list[key].lon], { icon: myIcon(list[key].price) })
       .addTo(map)
-      .on("click", () =>
-        L.popup()
-          .setLatLng([+list[key].lat, +list[key].lon])
-          .setContent(tooltip())
-          .openOn(map)
-      );
+      .bindPopup(tooltip(list[key]));
   }
 }
