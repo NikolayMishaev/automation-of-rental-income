4bae9c34a90049f17c819f0d4f7f3139e8b92ea0
Nikolay.Mishaev <nikolay.mishaev@x5.ru>
2022-04-19 07:42:18 +0300

    AWP-87 feat: Контент из вкладки "Аналитические данные" обернут в iframe

--- assets/js/manager/analytics.js
+++ assets/js/manager/analytics.js
@@ -502,234 +502,323 @@ function checkSubmenuSecondLevel(ariaLabel) {
 // диаграммы
 
 var canvas = document.getElementById("graph1");
-var ctx = canvas.getContext("2d");
+if (canvas) {
+    var ctx = canvas.getContext("2d");
+    var gradient = ctx.createLinearGradient(0, 0, 0, 400);
+    gradient.addColorStop(0, "#E6FAE9");
+    gradient.addColorStop(1, "#E6FAE900");
 
-var gradient = ctx.createLinearGradient(0, 0, 0, 400);
-gradient.addColorStop(0, "#E6FAE9");
-gradient.addColorStop(1, "#E6FAE900");
+    var data = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+        datasets: [
+            {
+                label: false,
+                backgroundColor: gradient,
+                borderColor: "#00796B",
+                borderWidth: 2,
+                hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                hoverBorderColor: "rgba(255,99,132,1)",
+                data: [65, 59, 50, 81, 56, 55, 40],
+                fill: true,
+                pointRadius: 0,
+            },
+        ],
+    };
 
-var data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-    datasets: [
-        {
-            label: false,
-            backgroundColor: gradient,
-            borderColor: "#00796B",
-            borderWidth: 2,
-            hoverBackgroundColor: "rgba(255,99,132,0.4)",
-            hoverBorderColor: "rgba(255,99,132,1)",
-            data: [65, 59, 50, 81, 56, 55, 40],
-            fill: true,
-            pointRadius: 0,
+    var options = {
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                stacked: true,
+                grid: {
+                    display: false,
+                    color: "rgba(255,99,132,0.2)",
+                },
+            },
+            x: {
+                grid: {
+                    display: false,
+                },
+            },
         },
-    ],
-};
+    };
 
-var options = {
-    maintainAspectRatio: false,
-    scales: {
-        y: {
-            stacked: true,
-            grid: {
+    var data2 = {
+        labels: [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+        ],
+        datasets: [
+            {
+                label: false,
+                backgroundColor: "#9CCC65",
+                borderRadius: 4,
+                hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
+                data: [
+                    65, 59, 50, 81, 56, 55, 40, 65, 59, 50, 81, 56, 55, 40, 65,
+                    59, 50, 81, 56, 55, 40,
+                ],
+                fill: true,
+            },
+        ],
+    };
+
+    var data3 = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+        datasets: [
+            {
+                label: false,
+                backgroundColor: "#9CCC65",
+                borderRadius: 4,
+                hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
+                data: [65, 59, 50, 81, 56, 55, 40],
+                fill: true,
+            },
+        ],
+    };
+
+    var data4 = {
+        labels: [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+        ],
+        datasets: [
+            {
+                label: "Обработано",
+                backgroundColor: "#55B465",
+                borderRadius: 4,
+                hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
+                data: [
+                    65, 59, 50, 81, 56, 55, 40, 65, 59, 20, 81, 56, 55, 40, 65,
+                    59, 20, 81, 56, 55, 40,
+                ],
+            },
+            {
+                label: "Отправлено менеджеру",
+                backgroundColor: "#FFEE58",
+                borderRadius: 4,
+                hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
+                pointStyle: "vircle",
+                data: [
+                    36, 72, 40, 81, 96, 45, 10, 95, 39, 30, 41, 96, 55, 80, 75,
+                    39, 10, 61, 86, 35, 55,
+                ],
+            },
+            {
+                label: "В работе у менеджера",
+                backgroundColor: "#5C6BC0",
+                borderRadius: 4,
+                hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
+                data: [
+                    56, 52, 50, 71, 46, 35, 20, 65, 79, 60, 91, 36, 65, 50, 95,
+                    19, 80, 21, 36, 75, 45,
+                ],
+            },
+        ],
+    };
+
+    var options = {
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
                 display: false,
-                color: "rgba(255,99,132,0.2)",
+                labels: {
+                    color: "rgb(255, 99, 132)",
+                },
             },
         },
-        x: {
-            grid: {
-                display: false,
+        scales: {
+            y: {
+                stacked: true,
+                grid: {
+                    display: false,
+                    color: "rgba(255,99,132,0.2)",
+                },
+            },
+            x: {
+                grid: {
+                    display: false,
+                },
             },
         },
-    },
-};
-
-var data2 = {
-    labels: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-    ],
-    datasets: [
-        {
-            label: false,
-            backgroundColor: "#9CCC65",
-            borderRadius: 4,
-            hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
-            data: [
-                65, 59, 50, 81, 56, 55, 40, 65, 59, 50, 81, 56, 55, 40, 65, 59,
-                50, 81, 56, 55, 40,
-            ],
-            fill: true,
-        },
-    ],
-};
-
-var data3 = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-    datasets: [
-        {
-            label: false,
-            backgroundColor: "#9CCC65",
-            borderRadius: 4,
-            hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
-            data: [65, 59, 50, 81, 56, 55, 40],
-            fill: true,
-        },
-    ],
-};
-
-var data4 = {
-    labels: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-    ],
-    datasets: [
-        {
-            label: "Обработано",
-            backgroundColor: "#55B465",
-            borderRadius: 4,
-            hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
-            data: [
-                65, 59, 50, 81, 56, 55, 40, 65, 59, 20, 81, 56, 55, 40, 65, 59,
-                20, 81, 56, 55, 40,
-            ],
-        },
-        {
-            label: "Отправлено менеджеру",
-            backgroundColor: "#FFEE58",
-            borderRadius: 4,
-            hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
-            pointStyle: "vircle",
-            data: [
-                36, 72, 40, 81, 96, 45, 10, 95, 39, 30, 41, 96, 55, 80, 75, 39,
-                10, 61, 86, 35, 55,
-            ],
-        },
-        {
-            label: "В работе у менеджера",
-            backgroundColor: "#5C6BC0",
-            borderRadius: 4,
-            hoverBackgroundColor: "rgba(156, 204, 101, 0.8)",
-            data: [
-                56, 52, 50, 71, 46, 35, 20, 65, 79, 60, 91, 36, 65, 50, 95, 19,
-                80, 21, 36, 75, 45,
-            ],
-        },
-    ],
-};
-
-var options = {
-    maintainAspectRatio: false,
-    plugins: {
-        legend: {
-            display: false,
-            labels: {
-                color: "rgb(255, 99, 132)",
+    };
+    var options2 = {
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: true,
+                labels: {
+                    color: "#3C4142",
+                },
+                position: "bottom",
+                align: "start",
+                labels: { usePointStyle: true },
             },
         },
-    },
-    scales: {
-        y: {
-            stacked: true,
-            grid: {
-                display: false,
-                color: "rgba(255,99,132,0.2)",
+        scales: {
+            y: {
+                stacked: true,
+                grid: {
+                    display: false,
+                    color: "rgba(255,99,132,0.2)",
+                },
+            },
+            x: {
+                stacked: true,
+                grid: {
+                    display: false,
+                },
             },
         },
-        x: {
-            grid: {
-                display: false,
-            },
-        },
-    },
-};
-var options2 = {
-    maintainAspectRatio: false,
-    plugins: {
-        legend: {
-            display: true,
-            labels: {
-                color: "#3C4142",
-            },
-            position: "bottom",
-            align: "start",
-            labels: { usePointStyle: true },
-        },
-    },
-    scales: {
-        y: {
-            stacked: true,
-            grid: {
-                display: false,
-                color: "rgba(255,99,132,0.2)",
-            },
-        },
-        x: {
-            stacked: true,
-            grid: {
-                display: false,
-            },
-        },
-    },
-};
+    };
 
-new Chart("graph1", {
-    type: "line",
-    options: options,
-    data: data,
-});
+    new Chart("graph1", {
+        type: "line",
+        options: options,
+        data: data,
+    });
 
-new Chart("graph2", {
-    type: "bar",
-    options: options,
-    data: data2,
-});
+    new Chart("graph2", {
+        type: "bar",
+        options: options,
+        data: data2,
+    });
 
-new Chart("graph3", {
-    type: "bar",
-    options: options,
-    data: data3,
-});
+    new Chart("graph3", {
+        type: "bar",
+        options: options,
+        data: data3,
+    });
 
-new Chart("graph4", {
-    type: "bar",
-    options: options2,
-    data: data4,
-});
+    new Chart("graph4", {
+        type: "bar",
+        options: options2,
+        data: data4,
+    });
+}
+
+// логика оценка звездочками во вкладке Аналитика
+
+const analyticalStarsTotal = document.querySelectorAll(".analytics-star-total");
+const analyticalStarsRangeFrom = document.querySelectorAll(
+    ".analytics-star-range-from"
+);
+const analyticalStarsRangeTo = document.querySelectorAll(
+    ".analytics-star-range-to"
+);
+const containerStars = document.querySelector("#container-analytics-stars");
+const inputAnalyticalStarsTotal = document.querySelector(
+    "#input-analytics-star-total"
+);
+const inputAnalyticalStarsRangeFrom = document.querySelector(
+    "#input-analytics-star-range-from"
+);
+const inputAnalyticalStarsRangeTo = document.querySelector(
+    "#input-analytics-star-range-to"
+);
+
+const buttonResetAllStars = document.querySelector(
+    "#analytics-stars-button-reset"
+);
+
+if (containerStars) {
+    containerStars.addEventListener("click", (e) => {
+        if (e.target.ariaLabel) {
+            if (e.target.classList.contains("analytics-star-total")) {
+                resetActiveClass(
+                    analyticalStarsTotal,
+                    "prof-control-panel__star_active"
+                );
+                addClassElementStars(
+                    analyticalStarsTotal,
+                    +e.target.ariaLabel,
+                    inputAnalyticalStarsTotal
+                );
+            }
+            if (e.target.classList.contains("analytics-star-range-from")) {
+                resetActiveClass(
+                    analyticalStarsRangeFrom,
+                    "prof-control-panel__star_active"
+                );
+                addClassElementStars(
+                    analyticalStarsRangeFrom,
+                    +e.target.ariaLabel,
+                    inputAnalyticalStarsRangeFrom
+                );
+            }
+            if (e.target.classList.contains("analytics-star-range-to")) {
+                resetActiveClass(
+                    analyticalStarsRangeTo,
+                    "prof-control-panel__star_active"
+                );
+                addClassElementStars(
+                    analyticalStarsRangeTo,
+                    +e.target.ariaLabel,
+                    inputAnalyticalStarsRangeTo
+                );
+            }
+        }
+    });
+
+    function resetActiveClass(arrayElements, className) {
+        arrayElements.forEach((i) => i.classList.remove(className));
+    }
+
+    function addClassElementStars(arrayStars, currentValue, input) {
+        arrayStars.forEach((i, c) => {
+            if (c < currentValue) {
+                i.classList.add("prof-control-panel__star_active");
+                input.value = currentValue;
+                return;
+            }
+        });
+    }
+
+    buttonResetAllStars.addEventListener("click", () => {
+        [
+            analyticalStarsTotal,
+            analyticalStarsRangeFrom,
+            analyticalStarsRangeTo,
+        ].forEach((i) =>
+            resetActiveClass(i, "prof-control-panel__star_active")
+        );
+    });
+}
