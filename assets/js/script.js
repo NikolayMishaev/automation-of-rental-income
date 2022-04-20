2ef5266e43c14918c60d617e2c62a4858857ec44
Nikolay.Mishaev <nikolay.mishaev@x5.ru>
2022-04-20 11:51:44 +0300

    AWP-87 feat Добавлен селект на странице регистрации

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,45 @@
-const state = { currentOpenPopup: null };
+// общие функции
+function addClassElement(element, className) {
+    element.classList.add(className);
+}
+
+function removeClassElement(element, className) {
+    element.classList.remove(className);
+}
+
+function switchButtons(
+    eventTarget,
+    arrayButtons,
+    arrayContent,
+    activeClassButton,
+    notActiveClassContent
+) {
+    if (eventTarget.closest(`.${activeClassButton}`)) {
+        return;
+    }
+    arrayButtons.forEach((i) => removeClassElement(i, activeClassButton));
+    addClassElement(eventTarget, activeClassButton);
+    arrayContent.forEach((i) => {
+        if (i.ariaLabel === eventTarget.ariaLabel) {
+            removeClassElement(i, notActiveClassContent);
+        } else {
+            addClassElement(i, notActiveClassContent);
+        }
+    });
+}
+
+const state = {
+    currentOpenPopup: null,
+    cursorsSelect: {
+        "view-document": document.querySelector("#cursor-view-document"),
+    },
+    submenuSelect: {
+        "view-document": document.querySelector("#submenu-view-document"),
+    },
+    inputsSelect: {
+        "view-document": document.querySelector("#input-view-document"),
+    },
+};
 const modalOfferPrice = document.querySelector(".modal-offer-price");
 const offerBtn = document.querySelectorAll(".info-card__offer-btn");
 const modalForm = document.querySelector(".modal-form");
@@ -714,4 +755,193 @@ function handleEscClose(e) {
             document.removeEventListener("keydown", handleEscClose);
         }
     }
+}
+
+// логика по работе селектов
+
+// проверить, где сработал клик, если за пределами тела селекта, то запустить ф-ию для скрытия текущего открытого подменю
+const checkClickOutsideSelect = (e) => {
+    // если клик произошел за пределами селекта(label)
+    if (!e.target.closest(".prof-control-panel__select-label")) {
+        // скрыть текущее подменю
+        hideCurrentSubmenu(state.currentOpenSubmenu, true);
+        hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, true);
+        state.currentOpenSubmenuSecondLevel = null;
+    }
+};
+
+function setInputValueByValueActiveCheckbox() {
+    if (
+        state.inputsSelect["checkbox-free"].checked &&
+        state.inputsSelect["checkbox-free-soon"].checked
+    ) {
+        state.inputsSelect["objects-status"].value = "Выбрано несколько";
+    } else if (state.inputsSelect["checkbox-free"].checked) {
+        state.inputsSelect["objects-status"].value = "Свободен";
+    } else if (state.inputsSelect["checkbox-free-soon"].checked) {
+        state.inputsSelect["objects-status"].value = "Скоро освободится";
+    } else {
+        state.inputsSelect["objects-status"].value = "Все";
+    }
+}
+
+function toggleVisibleSubmenuSecondLevel(currentLabel) {
+    if (
+        !currentLabel.classList.contains(
+            "prof-control-panel__select-label_active"
+        )
+    ) {
+        hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, false);
+        addClassElement(
+            state.submenuSelect[currentLabel.ariaLabel],
+            "mix-visible"
+        );
+        addClassElement(
+            currentLabel,
+            "prof-control-panel__select-label_active"
+        );
+        state.currentOpenSubmenuSecondLevel =
+            state.submenuSelect[currentLabel.ariaLabel];
+        if (state.cursorsSelect[currentLabel.ariaLabel]) {
+            addClassElement(
+                state.cursorsSelect[currentLabel.ariaLabel],
+                "prof-control-panel__cursor_active"
+            );
+        }
+    } else {
+        hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, false);
+        state.currentOpenSubmenuSecondLevel = null;
+    }
+}
+
+// установить слушатель клика на весь документ и отслеживать клик вне тела селекта
+function setListenerClickOutsideSelect() {
+    document.addEventListener("click", checkClickOutsideSelect);
+}
+
+// скрыть текущее открытое подменю селекта
+function hideCurrentSubmenu(submenu, deleteListenerOverlay) {
+    // если в стейте есть текущее открытое подменю
+    if (submenu) {
+        // найти по подменю текущий селект
+        const currentSelectLabel = submenu.closest(
+            ".prof-control-panel__select-label"
+        );
+        // удалить активный класс у текущего селекта
+        removeClassElement(
+            currentSelectLabel,
+            "prof-control-panel__select-label_active"
+        );
+        // удалить активный класс у текущего курсора
+        if (state.cursorsSelect[currentSelectLabel.ariaLabel]) {
+            removeClassElement(
+                state.cursorsSelect[currentSelectLabel.ariaLabel],
+                "prof-control-panel__cursor_active"
+            );
+        }
+        // скрыть текущее открытое подменю
+        removeClassElement(submenu, "mix-visible");
+        // очистить стейт
+
+        if (deleteListenerOverlay) {
+            // удалить слушатель document, т.к. все селекты закрыты
+            document.removeEventListener("click", checkClickOutsideSelect);
+        }
+    }
+}
+
+const buttonsSelect = document.querySelectorAll(".button-toggle-select");
+
+buttonsSelect.forEach((i) =>
+    i.addEventListener("click", (e) => {
+        const currentLabel = e.target.closest(
+            ".prof-control-panel__select-label"
+        );
+        if (e.target.closest(".prof-control-panel__label-custom")) {
+            const currentCheckedValue = e.target
+                .closest(".prof-control-panel__label-custom")
+                .textContent.trim();
+            state.inputsSelect[currentLabel.ariaLabel].value =
+                currentCheckedValue;
+            return;
+        }
+        if (
+            e.target.closest(".label-checkbox") &&
+            e.target.closest(".label-checkbox").ariaLabel === "checkbox"
+        ) {
+            setInputValueByValueActiveCheckbox();
+            return;
+        }
+        if (e.target.ariaLabel === "item") {
+            state.inputsSelect[currentLabel.ariaLabel].value =
+                e.target.textContent.trim();
+            Array.from(
+                e.target.closest(".main-submenu__list").children
+            ).forEach((i) =>
+                removeClassElement(i, "main-submenu__item_active")
+            );
+            addClassElement(
+                e.target.closest(".main-submenu__item"),
+                "main-submenu__item_active"
+            );
+            if (state.currentOpenSubmenuSecondLevel) {
+                hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, false);
+            } else {
+                hideCurrentSubmenu(state.currentOpenSubmenu, true);
+            }
+
+            return;
+        }
+        if (e.target.classList.contains("prof-control-panel__button")) {
+            hideCurrentSubmenu(state.currentOpenSubmenu, true);
+            return;
+        }
+        if (e.target.closest(".main-submenu")) {
+            const currentLabelSecondLevel = e.target.closest(
+                ".prof-control-panel__select-label"
+            );
+            if (
+                currentLabelSecondLevel &&
+                checkSubmenuSecondLevel(currentLabelSecondLevel.ariaLabel)
+            ) {
+                toggleVisibleSubmenuSecondLevel(currentLabelSecondLevel);
+            }
+            return;
+        }
+        if (currentLabel) {
+            if (
+                !currentLabel.classList.contains(
+                    "prof-control-panel__select-label_active"
+                )
+            ) {
+                hideCurrentSubmenu(state.currentOpenSubmenu, true);
+                hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, true);
+                addClassElement(
+                    state.submenuSelect[currentLabel.ariaLabel],
+                    "mix-visible"
+                );
+                state.currentOpenSubmenu =
+                    state.submenuSelect[currentLabel.ariaLabel];
+                setListenerClickOutsideSelect();
+                addClassElement(
+                    currentLabel,
+                    "prof-control-panel__select-label_active"
+                );
+                if (state.cursorsSelect[currentLabel.ariaLabel]) {
+                    addClassElement(
+                        state.cursorsSelect[currentLabel.ariaLabel],
+                        "prof-control-panel__cursor_active"
+                    );
+                }
+            } else {
+                hideCurrentSubmenu(state.currentOpenSubmenu, true);
+                hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, true);
+                state.currentOpenSubmenuSecondLevel = null;
+            }
+        }
+    })
+);
+
+function checkSubmenuSecondLevel(ariaLabel) {
+    return ariaLabel.includes("division") || ariaLabel.includes("structure");
 }
