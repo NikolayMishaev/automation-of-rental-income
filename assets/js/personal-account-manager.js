13e430b6a894633aeea995eb2b9e65c70ed8d6fd
Nikolay.Mishaev <nikolay.mishaev@x5.ru>
2022-04-20 14:29:00 +0300

    AWP-87 feat Переделана вкладка Лист ожидания

--- assets/js/personal-account-manager.js
+++ assets/js/personal-account-manager.js
@@ -18,6 +18,7 @@ const state = {
         "type-activity": document.querySelector("#cursor-type-activity"),
         "ankets-status": document.querySelector("#cursor-ankets-status"),
         "ankets-views": document.querySelector("#cursor-ankets-views"),
+        "waiting-list": document.querySelector("#cursor-waiting-list"),
         "objects-status": document.querySelector("#cursor-objects-status"),
         "objects-sort": document.querySelector("#cursor-objects-sort"),
         "objects-page": document.querySelector("#cursor-objects-page"),
@@ -77,6 +78,7 @@ const state = {
         "type-activity": document.querySelector("#submenu-type-activity"),
         "ankets-status": document.querySelector("#submenu-ankets-status"),
         "ankets-views": document.querySelector("#submenu-ankets-views"),
+        "waiting-list": document.querySelector("#submenu-waiting-list"),
         "objects-status": document.querySelector("#submenu-objects-status"),
         "objects-sort": document.querySelector("#submenu-objects-sort"),
         "objects-page": document.querySelector("#submenu-objects-page"),
@@ -146,6 +148,7 @@ const state = {
         "type-activity": document.querySelector("#input-type-activity"),
         "ankets-status": document.querySelector("#input-ankets-status"),
         "ankets-views": document.querySelector("#input-ankets-views"),
+        "waiting-list": document.querySelector("#input-waiting-list"),
         "checkbox-free": document.querySelector("#input-objects-checkobx-free"),
         "checkbox-free-soon": document.querySelector(
             "#input-objects-checkobx-free-soon"
@@ -450,7 +453,7 @@ function handleEscClose(e) {
 // логика наведения на текст в списке
 
 const listTable = document.querySelectorAll(
-    "#objects .prof-table__row-content.heading-h5, #offers .prof-table__row-content.heading-h5, #analytical-data .prof-table__row-content.heading-h5, .prof-table__row-content_type_agent.heading-h5"
+    ".prof-table__row-content.heading-h5"
 );
 
 listTable.forEach((i) =>
@@ -474,3 +477,192 @@ listTable.forEach((i) =>
         if (tooltip) tooltip.remove();
     })
 );
+
+// логика по работе селектов
+
+// проверить, где сработал клик, если за пределами тела селекта, то запустить ф-ию для скрытия текущего открытого подменю
+const checkClickOutsideSelect = (e) => {
+    // если клик произошел за пределами селекта(label)
+    if (!e.target.closest(".prof-control-panel__select-label")) {
+        // скрыть текущее подменю
+        hideCurrentSubmenu(state.currentOpenSubmenu, true);
+        hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, true);
+        state.currentOpenSubmenuSecondLevel = null;
+    }
+};
+
+function setInputValueByValueActiveCheckbox() {
+    if (
+        state.inputsSelect["checkbox-free"].checked &&
+        state.inputsSelect["checkbox-free-soon"].checked
+    ) {
+        state.inputsSelect["objects-status"].value = "Выбрано несколько";
+    } else if (state.inputsSelect["checkbox-free"].checked) {
+        state.inputsSelect["objects-status"].value = "Свободен";
+    } else if (state.inputsSelect["checkbox-free-soon"].checked) {
+        state.inputsSelect["objects-status"].value = "Скоро освободится";
+    } else {
+        state.inputsSelect["objects-status"].value = "Все";
+    }
+}
+
+function toggleVisibleSubmenuSecondLevel(currentLabel) {
+    if (
+        !currentLabel.classList.contains(
+            "prof-control-panel__select-label_active"
+        )
+    ) {
+        hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, false);
+        addClassElement(
+            state.submenuSelect[currentLabel.ariaLabel],
+            "mix-visible"
+        );
+        addClassElement(
+            currentLabel,
+            "prof-control-panel__select-label_active"
+        );
+        state.currentOpenSubmenuSecondLevel =
+            state.submenuSelect[currentLabel.ariaLabel];
+        if (state.cursorsSelect[currentLabel.ariaLabel]) {
+            addClassElement(
+                state.cursorsSelect[currentLabel.ariaLabel],
+                "prof-control-panel__cursor_active"
+            );
+        }
+    } else {
+        hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, false);
+        state.currentOpenSubmenuSecondLevel = null;
+    }
+}
+
+// установить слушатель клика на весь документ и отслеживать клик вне тела селекта
+function setListenerClickOutsideSelect() {
+    document.addEventListener("click", checkClickOutsideSelect);
+}
+
+// скрыть текущее открытое подменю селекта
+function hideCurrentSubmenu(submenu, deleteListenerOverlay) {
+    // если в стейте есть текущее открытое подменю
+    if (submenu) {
+        // найти по подменю текущий селект
+        const currentSelectLabel = submenu.closest(
+            ".prof-control-panel__select-label"
+        );
+        // удалить активный класс у текущего селекта
+        removeClassElement(
+            currentSelectLabel,
+            "prof-control-panel__select-label_active"
+        );
+        // удалить активный класс у текущего курсора
+        if (state.cursorsSelect[currentSelectLabel.ariaLabel]) {
+            removeClassElement(
+                state.cursorsSelect[currentSelectLabel.ariaLabel],
+                "prof-control-panel__cursor_active"
+            );
+        }
+        // скрыть текущее открытое подменю
+        removeClassElement(submenu, "mix-visible");
+        // очистить стейт
+
+        if (deleteListenerOverlay) {
+            // удалить слушатель document, т.к. все селекты закрыты
+            document.removeEventListener("click", checkClickOutsideSelect);
+        }
+    }
+}
+
+const buttonsSelect = document.querySelectorAll(".button-toggle-select");
+
+buttonsSelect.forEach((i) =>
+    i.addEventListener("click", (e) => {
+        const currentLabel = e.target.closest(
+            ".prof-control-panel__select-label"
+        );
+        if (e.target.closest(".prof-control-panel__label-custom")) {
+            const currentCheckedValue = e.target
+                .closest(".prof-control-panel__label-custom")
+                .textContent.trim();
+            state.inputsSelect[currentLabel.ariaLabel].value =
+                currentCheckedValue;
+            return;
+        }
+        if (
+            e.target.closest(".label-checkbox") &&
+            e.target.closest(".label-checkbox").ariaLabel === "checkbox"
+        ) {
+            setInputValueByValueActiveCheckbox();
+            return;
+        }
+        if (e.target.ariaLabel === "item") {
+            state.inputsSelect[currentLabel.ariaLabel].value =
+                e.target.textContent.trim();
+            Array.from(
+                e.target.closest(".main-submenu__list").children
+            ).forEach((i) =>
+                removeClassElement(i, "main-submenu__item_active")
+            );
+            addClassElement(
+                e.target.closest(".main-submenu__item"),
+                "main-submenu__item_active"
+            );
+            if (state.currentOpenSubmenuSecondLevel) {
+                hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, false);
+            } else {
+                hideCurrentSubmenu(state.currentOpenSubmenu, true);
+            }
+
+            return;
+        }
+        if (e.target.classList.contains("prof-control-panel__button")) {
+            hideCurrentSubmenu(state.currentOpenSubmenu, true);
+            return;
+        }
+        if (e.target.closest(".main-submenu")) {
+            const currentLabelSecondLevel = e.target.closest(
+                ".prof-control-panel__select-label"
+            );
+            if (
+                currentLabelSecondLevel &&
+                checkSubmenuSecondLevel(currentLabelSecondLevel.ariaLabel)
+            ) {
+                toggleVisibleSubmenuSecondLevel(currentLabelSecondLevel);
+            }
+            return;
+        }
+        if (currentLabel) {
+            if (
+                !currentLabel.classList.contains(
+                    "prof-control-panel__select-label_active"
+                )
+            ) {
+                hideCurrentSubmenu(state.currentOpenSubmenu, true);
+                hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, true);
+                addClassElement(
+                    state.submenuSelect[currentLabel.ariaLabel],
+                    "mix-visible"
+                );
+                state.currentOpenSubmenu =
+                    state.submenuSelect[currentLabel.ariaLabel];
+                setListenerClickOutsideSelect();
+                addClassElement(
+                    currentLabel,
+                    "prof-control-panel__select-label_active"
+                );
+                if (state.cursorsSelect[currentLabel.ariaLabel]) {
+                    addClassElement(
+                        state.cursorsSelect[currentLabel.ariaLabel],
+                        "prof-control-panel__cursor_active"
+                    );
+                }
+            } else {
+                hideCurrentSubmenu(state.currentOpenSubmenu, true);
+                hideCurrentSubmenu(state.currentOpenSubmenuSecondLevel, true);
+                state.currentOpenSubmenuSecondLevel = null;
+            }
+        }
+    })
+);
+
+function checkSubmenuSecondLevel(ariaLabel) {
+    return ariaLabel.includes("division") || ariaLabel.includes("structure");
+}
